8135366fed3a96521ccbbe8d6617e44c78f92a49
Parakhina Maria <mickeymouse0243@gmail.com>
2020-03-26 16:49:41 +0200

    lesson01

---
+++ script.js
@@ -0,0 +1,10 @@
+const money = 'money';
+const income = 'income';
+const addExpenses = 'addExpenses';
+const deposit = 'deposit';
+let mission = 'mission';
+var period = 'period';
+
+alert('Hello World')
+ 
+console.log("It works");
