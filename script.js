d07683d0cb54b35b1a6b63ff24fa816009bbbca2
JasperLi404 <59822486+JasperLi404@users.noreply.github.com>
2020-03-27 16:14:34 +0200

    harderLevel02

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const money = 'money';
-const income = 'income';
-const addExpenses = 'addExpenses';
-const deposit = 'deposit';
-let mission = 'mission';
-var period = 'period';
-
-alert('Hello World')
- 
-console.log("It works");
+let num = 266219;
+num = String(num).split('');
+let len = num.length;
+let i=0;
+let Dob = 1;
+while(i<len){
+    Dob *= num[i];
+    i++;
+}
+console.log(Dob);
+let pow = Dob
+let n = 3;
+while(--n){
+    pow*=Dob;
+}
+pow = String(pow);
+pow = pow.substr(0,2);
+console.log(pow);
