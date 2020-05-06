4bee555f8472bfa08598c264567817bfc5404358
Parakhina Maria <mickeymouse0243@gmail.com>
2020-05-06 18:22:18 +0300

    lesson27 / fetch

--- script.js
+++ script.js
@@ -351,7 +351,10 @@ window.addEventListener('DOMContentLoaded', () => {
                 body[key] = val;
             });
             POSTData(body)
-            .then(() => {
+            .then((response) => {
+                if(response.status !== 200){
+                    throw new Error('netvork status isn`t 200');
+                }
                 clearInput();
                 statusMessage.textContent = successMessage;
             })
@@ -365,186 +368,194 @@ window.addEventListener('DOMContentLoaded', () => {
             elementsForm.forEach(item => item.value = '')
         }
         
-        const animateLoading = () => {
-            const style = document.createElement('style');
-            document.head.appendChild(style);
-            statusMessage.classList.add('cover');
-            statusMessage.innerHTML = `
-            <div class="container">
-                <div class="dash uno"></div>
-                <div class="dash dos"></div>
-                <div class="dash tres"></div>
-                <div class="dash cuatro"></div>
-            </div>`;           
-            style.innerHTML = `
-            .cover{
-                width :100%;
-                display: flex;
-                flex-direction:row;
-                align-items:center;
-                justify-content: center;
-            }
-            .container{
-                display: flex;
-                flex-direction:row;
-                width: 220px;
-                height: 60px;
-            }
-              .dash {
-                margin: 0 15px;
-                width: 35px;
-                height: 15px;
-                border-radius: 8px;
-                background: #099dda;
-                box-shadow: 0 0 10px 0 #099dda;
-              }
+        // const animateLoading = () => {
+        //     const style = document.createElement('style');
+        //     document.head.appendChild(style);
+        //     statusMessage.classList.add('cover');
+        //     statusMessage.innerHTML = `
+        //     <div class="container">
+        //         <div class="dash uno"></div>
+        //         <div class="dash dos"></div>
+        //         <div class="dash tres"></div>
+        //         <div class="dash cuatro"></div>
+        //     </div>`;           
+        //     style.innerHTML = `
+        //     .cover{
+        //         width :100%;
+        //         display: flex;
+        //         flex-direction:row;
+        //         align-items:center;
+        //         justify-content: center;
+        //     }
+        //     .container{
+        //         display: flex;
+        //         flex-direction:row;
+        //         width: 220px;
+        //         height: 60px;
+        //     }
+        //       .dash {
+        //         margin: 0 15px;
+        //         width: 35px;
+        //         height: 15px;
+        //         border-radius: 8px;
+        //         background: #099dda;
+        //         box-shadow: 0 0 10px 0 #099dda;
+        //       }
               
-              .uno {
-                margin-right: -18px;
-                transform-origin: center left;
-                animation: spin 3s linear infinite;  
-              }
+        //       .uno {
+        //         margin-right: -18px;
+        //         transform-origin: center left;
+        //         animation: spin 3s linear infinite;  
+        //       }
               
-              .dos {
-                transform-origin: center right;
-                animation: spin2 3s linear infinite;
-                animation-delay: .2s;
-              }
+        //       .dos {
+        //         transform-origin: center right;
+        //         animation: spin2 3s linear infinite;
+        //         animation-delay: .2s;
+        //       }
               
-              .tres {
-                transform-origin: center right;
-                animation: spin3 3s linear infinite;
-                animation-delay: .3s;
-              }
+        //       .tres {
+        //         transform-origin: center right;
+        //         animation: spin3 3s linear infinite;
+        //         animation-delay: .3s;
+        //       }
               
-              .cuatro {
-                transform-origin: center right;
-                animation: spin4 3s linear infinite;
-                animation-delay: .4s;
-              }
+        //       .cuatro {
+        //         transform-origin: center right;
+        //         animation: spin4 3s linear infinite;
+        //         animation-delay: .4s;
+        //       }
               
-              @keyframes spin {
-                0% {
-                  transform: rotate(0deg);
-                }
-                25% {
-                  transform: rotate(360deg);
-                }
-                30% {
-                  transform: rotate(370deg);
-                }
-                35% {
-                  transform: rotate(360deg);
-                }
-                100% {
-                  transform: rotate(360deg);
-                }
-              }
+        //       @keyframes spin {
+        //         0% {
+        //           transform: rotate(0deg);
+        //         }
+        //         25% {
+        //           transform: rotate(360deg);
+        //         }
+        //         30% {
+        //           transform: rotate(370deg);
+        //         }
+        //         35% {
+        //           transform: rotate(360deg);
+        //         }
+        //         100% {
+        //           transform: rotate(360deg);
+        //         }
+        //       }
               
-              @keyframes spin2 {
-                0% {
-                  transform: rotate(0deg);
-                }
-                20% {
-                  transform: rotate(0deg);
-                }
-                30% {
-                  transform: rotate(-180deg);
-                }
-                35% {
-                  transform: rotate(-190deg);
-                }
-                40% {
-                  transform: rotate(-180deg);
-                }
-                78% {
-                  transform: rotate(-180deg);
-                }
-                95% {
-                  transform: rotate(-360deg);
-                }
-                98% {
-                  transform: rotate(-370deg);
-                }
-                100% {
-                  transform: rotate(-360deg);
-                }
-              }
+        //       @keyframes spin2 {
+        //         0% {
+        //           transform: rotate(0deg);
+        //         }
+        //         20% {
+        //           transform: rotate(0deg);
+        //         }
+        //         30% {
+        //           transform: rotate(-180deg);
+        //         }
+        //         35% {
+        //           transform: rotate(-190deg);
+        //         }
+        //         40% {
+        //           transform: rotate(-180deg);
+        //         }
+        //         78% {
+        //           transform: rotate(-180deg);
+        //         }
+        //         95% {
+        //           transform: rotate(-360deg);
+        //         }
+        //         98% {
+        //           transform: rotate(-370deg);
+        //         }
+        //         100% {
+        //           transform: rotate(-360deg);
+        //         }
+        //       }
               
-              @keyframes spin3 {
-                0% {
-                  transform: rotate(0deg);
-                }
-                27% {
-                  transform: rotate(0deg);  
-                }
-                40% {
-                  transform: rotate(180deg);
-                }
-                45% {
-                  transform: rotate(190deg);
-                }
-                50% {
-                  transform: rotate(180deg);
-                }
-                62% {
-                  transform: rotate(180deg);
-                }
-                75% {
-                  transform: rotate(360deg);
-                }
-                80% {
-                  transform: rotate(370deg);
-                }
-                85% {
-                  transform: rotate(360deg);
-                }
-                100% {
-                  transform: rotate(360deg);
-                }
-              }
+        //       @keyframes spin3 {
+        //         0% {
+        //           transform: rotate(0deg);
+        //         }
+        //         27% {
+        //           transform: rotate(0deg);  
+        //         }
+        //         40% {
+        //           transform: rotate(180deg);
+        //         }
+        //         45% {
+        //           transform: rotate(190deg);
+        //         }
+        //         50% {
+        //           transform: rotate(180deg);
+        //         }
+        //         62% {
+        //           transform: rotate(180deg);
+        //         }
+        //         75% {
+        //           transform: rotate(360deg);
+        //         }
+        //         80% {
+        //           transform: rotate(370deg);
+        //         }
+        //         85% {
+        //           transform: rotate(360deg);
+        //         }
+        //         100% {
+        //           transform: rotate(360deg);
+        //         }
+        //       }
               
-              @keyframes spin4 {
-                0% {
-                  transform: rotate(0deg);
-                }
-                38% {
-                  transform: rotate(0deg);
-                }
-                60% {
-                  transform: rotate(-360deg);
-                }
-                65% {
-                  transform: rotate(-370deg);
-                }
-                75% {
-                  transform: rotate(-360deg);
-                }
-                100% {
-                  transform: rotate(-360deg);
-                }
-              }
-              `;
-        }
+        //       @keyframes spin4 {
+        //         0% {
+        //           transform: rotate(0deg);
+        //         }
+        //         38% {
+        //           transform: rotate(0deg);
+        //         }
+        //         60% {
+        //           transform: rotate(-360deg);
+        //         }
+        //         65% {
+        //           transform: rotate(-370deg);
+        //         }
+        //         75% {
+        //           transform: rotate(-360deg);
+        //         }
+        //         100% {
+        //           transform: rotate(-360deg);
+        //         }
+        //       }
+        //       `;
+        
+        //     }
         const POSTData = (body) => {
-            return new Promise((resolve, reject) => {
-                const request = new XMLHttpRequest();
-                request.addEventListener('readystatechange', () =>{
-                    animateLoading();
-                    if(request.readyState !== 4){
-                        return;
-                    }
-                    if(request.status === 200){
-                        
-                        return resolve();
-                    }else{
-                        return reject();
-                    }
-                } );
-                request.open('POST', './server.php');
-                request.setRequestHeader('Content-Type', 'application/json');
-                request.send(JSON.stringify(body));
+            return fetch('./server.php', {
+                method : 'POST',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(body)
             });
+            // return new Promise((resolve, reject) => {
+            //     const request = new XMLHttpRequest();
+            //     request.addEventListener('readystatechange', () =>{
+            //         // animateLoading();
+            //         if(request.readyState !== 4){
+            //             return;
+            //         }
+            //         if(request.status === 200){
+                        
+            //             return resolve();
+            //         }else{
+            //             return reject();
+            //         }
+            //     } );
+            //     request.open('POST', './server.php');
+            //     request.setRequestHeader('Content-Type', 'application/json');
+            //     request.send(JSON.stringify(body));
+            // });
         };
     }
     sendForm('form1');
